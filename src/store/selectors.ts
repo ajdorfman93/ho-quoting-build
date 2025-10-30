8d7958d09a13340bb9394ec7ac7578e72f22b835
ajdorfman93 <dorfmanart@gmail.com>
2025-10-30 14:55:21 -0400

    54

--- src/store/selectors.ts
+++
@@ -1,7 +0,0 @@
-import { shallow } from 'zustand/shallow';
-import { useStore } from './index';
-
-export const useParams = () => useStore((s) => s.params, shallow);
-export const useDoorSwing = () => useStore((s) => s.params.doorSwingDeg);
-export const useGizmoMode = () => useStore((s) => s.transforms.gizmoMode);
-export const useActiveAgent = () => useStore((s) => s.transforms.activeAgent);
